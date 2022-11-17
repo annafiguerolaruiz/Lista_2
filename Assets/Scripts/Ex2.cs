87263fe0fd3321326fdce0264c4e4589ea61d34e
annafiguerolaruiz <91455277+annafiguerolaruiz@users.noreply.github.com>
2022-11-17 17:05:52 +0100

    as

--- Assets/Scripts/Ex2.cs
+++ Assets/Scripts/Ex2.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Ex2 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   
+    public float Counter; 
+    public TextMeshProUGUI TEXTtime;
+    public Image IMAGEtime;
 
-    // Update is called once per frame
+    private float FirstTime;
+    private bool press = true;
+
+    
+    public void StartButton()
+    {
+        //Can't pause
+        if (press == true) 
+        {
+            FirstTime = Counter;
+            TEXTtime.text = Counter.ToString();
+            press = false;
+        }
+    }
     void Update()
     {
-        
+        //Time runs after pressing
+        if (Counter > 0 && press == false)
+        {
+            UpdateTime();
+        }
     }
+
+    public void UpdateTime()
+    {
+        Counter -= Time.deltaTime;
+        //No milliseconds
+        TEXTtime.text = Mathf.Round(Counter).ToString();     
+        IMAGEtime.fillAmount = Counter / FirstTime;
+    }
+
 }
